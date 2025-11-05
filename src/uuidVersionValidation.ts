b0a3f9e93fb676a73ec90947206952a03f72d5df
TCotton <me@andywalpole.me>
2025-11-05 18:17:12 +0000

    added optional versionNumber parameter to uuidVersionValidation

--- src/uuidVersionValidation.ts
+++ src/uuidVersionValidation.ts
@@ -1,36 +1,52 @@
-import { uuidRegex } from './uuidRegex.js';
+import {uuidRegex} from './uuidRegex.js';
+
 type UUIDVersionTuple =
-  | 'v1'
-  | 'v2'
-  | 'v3'
-  | 'v4'
-  | 'v5'
-  | 'v6'
-  | 'v7'
-  | 'v8'
-  | 'NilUUID'
-  | 'MaxUUID'
-  | undefined;
+    | 'v1'
+    | 'v2'
+    | 'v3'
+    | 'v4'
+    | 'v5'
+    | 'v6'
+    | 'v7'
+    | 'v8'
+    | 'NilUUID'
+    | 'MaxUUID'
+    | undefined
+    | true
+    | false;
 
-const uuidVersionValidation = (uuid: string): UUIDVersionTuple => {
-  const match: RegExpMatchArray | null = uuidRegex(uuid);
+type Version = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+type UUID = string
 
-  if (match) {
-    // Extract the version from the UUID (13th character, or index 14 in the string with hyphens)
-    const version = uuid.charAt(14);
+interface UUIDVersion {
+    versionNumber: Version;
+    uuid: UUID;
+}
 
-    return `v${version}` as UUIDVersionTuple;
-  }
+const uuidVersionValidation = (uuid: UUIDVersion["uuid"], versionNumber?: number): UUIDVersionTuple => {
+    const match: RegExpMatchArray | null = uuidRegex(uuid);
 
-  if (uuid === '00000000-0000-0000-0000-000000000000') {
-    return 'NilUUID' as UUIDVersionTuple;
-  }
+    if (match && versionNumber) {
+        const version = uuid.charAt(14);
+        return (Number(version) === versionNumber)
+    }
 
-  if (uuid.toLowerCase() === 'ffffffff-ffff-ffff-ffff-ffffffffffff') {
-    return 'MaxUUID' as UUIDVersionTuple;
-  }
+    if (match) {
+        // Extract the version from the UUID (13th character, or index 14 in the string with hyphens)
+        const version = uuid.charAt(14);
 
-  return undefined;
+        return `v${version}` as UUIDVersionTuple;
+    }
+
+    if (uuid === '00000000-0000-0000-0000-000000000000') {
+        return 'NilUUID' as UUIDVersionTuple;
+    }
+
+    if (uuid.toLowerCase() === 'ffffffff-ffff-ffff-ffff-ffffffffffff') {
+        return 'MaxUUID' as UUIDVersionTuple;
+    }
+
+    return undefined;
 };
 
-export { uuidVersionValidation, type UUIDVersionTuple };
+export {uuidVersionValidation, type UUIDVersionTuple};
