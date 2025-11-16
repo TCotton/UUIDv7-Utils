9504046b4a8c814ccf0aed2e0013d2f4b9cea519
TCotton <me@andywalpole.me>
2025-11-16 16:13:02 +0000

    feat: added handleBuffer function with unit tests

---
+++ src/handleBuffer.ts
@@ -0,0 +1,9 @@
+import { isBuffer } from './isBuffer.js';
+import { stringify } from './stringify.js';
+
+export const handleBuffer = (value: string | Buffer) => {
+    if (isBuffer(value)) {
+        return stringify(value);
+    }
+    return value;
+}
